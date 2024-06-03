0779bb5f5c97de1b4262b6e5a6a3eee3bedfcf19
Cascade0507 <tejaslovespancakes0507@gmail.com>
2024-06-03 20:14:59 +0530

    add compression using DRACO

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import { Canvas } from '@react-three/fiber'
-import './App.css'
-import { OrbitControls } from '@react-three/drei'
+
+import React from "react";
+import { Canvas } from "@react-three/fiber" 
+import "./App.css";
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+import { OrbitControls } from "@react-three/drei";
+import { Stats } from "@react-three/drei";
+import { DRACOLoader } from "three/examples/jsm/Addons.js";
+
+function Scene() {
+  const gltf = useLoader(GLTFLoader, path, (loader) => {
+    const dracoLoader = new DRACOLoader()
+    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/')
+    loader.setDRACOLoader(dracoLoader)
+  })
+  return <primitive object={gltf.scene} />
+}
+
 
 
 export default function App() {
-  return (
-    <>
-      <Canvas camera={{ position: [-8, 5, 8] }}>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh scale={3}>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
-        <OrbitControls />
-      </Canvas>
-    </>
-  )
-}
+  return(
+    
+    <Canvas style={{ background: "#171717" }}>
+      <OrbitControls> </OrbitControls>
+      <ambientLight />  
+      <Scene />
+      <Stats />
+    </Canvas>
+)};
