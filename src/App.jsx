6894a5e0193a3290c680752341653cf4de1ef04b
RishikMakam <118440544+RishikMakam@users.noreply.github.com>
2024-06-03 14:47:03 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { Canvas } from '@react-three/fiber'
-import './App.css'
-import { OrbitControls } from '@react-three/drei'
+import { Canvas, useLoader } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Suspense } from 'react';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import './App.css'; // Import App.css here
+import {Stats} from '@react-three/drei'
 
-
+function Model() {
+  const gltf = useLoader(GLTFLoader, '/free_1975_porsche_911_930_turbo.glb');
+  return <primitive object={gltf.scene} />;
+}
 export default function App() {
   return (
     <>
       <Canvas camera={{ position: [-8, 5, 8] }}>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh scale={3}>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
+      <ambientLight intensity={10}  />
+        <directionalLight color="white" intensity={19} position={[4, 1, 1]} />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
         <OrbitControls />
+        <Stats/>
       </Canvas>
     </>
-  )
+  );
 }
