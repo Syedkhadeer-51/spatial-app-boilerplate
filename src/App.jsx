e239b6fc0dc059648aee2125d0d0239425573b00
Bhavana <bhavana.bandi@gmail.com>
2024-06-03 11:13:00 +0530

    Changed the colours

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     <>
       <Canvas camera={{ position: [-8, 5, 8] }}>
         <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
+        <directionalLight color="white" position={[0, 0, 5]} />
         <mesh scale={3}>
           <boxGeometry />
           <meshStandardMaterial />
