4c8b206cd74682da42f4f4f77f2bdd26dfe7432f
Cascade0507 <tejaslovespancakes0507@gmail.com>
2024-06-10 22:17:54 +0530

    make animation changes to sphere, fix buggy and unintentional animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,19 @@
+import { Canvas } from "@react-three/fiber";
+import { Leva } from "leva";
+import { Experience } from "./components/Experience";
+import { Overlay } from "./components/Overlay";
 
-import React from "react";
-import { Canvas } from "@react-three/fiber" 
-import * as THREE from 'three';
-import "./App.css";
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { useRef } from 'react'
-import { DragControls } from "@react-three/drei";
-import { OrbitControls } from "@react-three/drei";
-import { Stats } from "@react-three/drei";
-import { DRACOLoader } from "three/examples/jsm/Addons.js";
-import { positionGeometry } from "three/examples/jsm/nodes/Nodes.js";
-
-function Scene() {
-  
-  const path1 = "/brick_arcs_barn_ruin-transformed.glb"
-  const path2 = "/brick_arcs_barn_ruin.glb"
-  const comp="compressed"
-  const ncomp="not compressed"
-  handleExport(path2,ncomp);
-  handleExport(path1,comp);
-  const gltf = useLoader(GLTFLoader,path1, (loader) => {
-    const dracoLoader = new DRACOLoader()
-    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/')
-    loader.setDRACOLoader(dracoLoader)
-  })
-  return <primitive object={gltf.scene} />
+function App() {
+  return (
+    <>
+      <Leva hidden />
+      <Overlay />
+      <Canvas shadows camera={{ position: [0, 0, 5], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Experience />
+      </Canvas>
+    </>
+  );
 }
 
-
-
-export default function App() {
-  return(
-    
-    <Canvas style={{ background: "#dddddd" }} >
-      <OrbitControls> </OrbitControls>
-      <ambientLight /> 
-      <pointLight position={[0,10,0]}></pointLight> 
-      <Scene />
-      <Stats />
-      <DragControls />
-    </Canvas>
-)};
-
-const handleExport = (glbModelUrl,comp) => {
-  fetch(glbModelUrl)
-      .then(response => response.blob())
-      .then(blob => {
-          // GLB model as a Blob
-          console.log("Size of",comp ,"GLB model:", blob.size, "bytes");
-        
-      })
-      .catch(error => {
-          console.error('Error fetching GLB model:', error);
-        });
-};
+export default App;
