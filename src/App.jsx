7e05b11c467074637d1a938cbc331ec5645ac20c
Aadishesh Udupa K <116788957+Aadisheshudupa@users.noreply.github.com>
2024-06-12 11:02:07 +0530

    coomit regarding updated compression

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
-import { Canvas} from '@react-three/fiber'
-import './App.css'
-import  {Stats} from '@react-three/drei'
-import { useState,useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import './App.css';
+import { Stats, OrbitControls } from '@react-three/drei';
+import { useState, useRef, useCallback } from 'react';
 import Compression from './Compression.jsx';
-import { OrbitControls } from '@react-three/drei';
+
 export default function App() {
+    const [modelPath, setModelPath] = useState(null);
+    const exportHandlerRef = useRef(null);
 
-  const [backgroundColor, setBackgroundColor] = useState('#3b3b3b');
-  const [gridColor, setGridColor] = useState('#ffffff');
-  const gridHelperRef = useRef(null);
+    const handleFileUpload = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const url = URL.createObjectURL(file);
+            setModelPath(url);
+            console.log("Original File: " + file.size);
+        }
+    };
 
-  
- 
-  return (
-    <>
-      <Canvas camera={{position: [0, 3, 10]} }>
-        <ambientLight/>
-        <Compression/>
-        <OrbitControls/>
-      <Stats/>
-      </Canvas>
-    </>
-  );
+    const setExportHandler = useCallback((handler) => {
+        exportHandlerRef.current = handler;
+    }, []);
+
+    const handleExportClick = () => {
+        if (exportHandlerRef.current) {
+            exportHandlerRef.current();
+        }
+    };
+
+    return (
+        <>
+            <input type="file" accept=".glb, .gltf" onChange={handleFileUpload} style={{ position:'absolute', top:'100px', zIndex:'1'}} className="custom-file-upload"   />
+            <button onClick={handleExportClick} style={{position:'absolute', top:'150px', zIndex:'1'}}>Export Compressed Model</button>
+            <Canvas camera={{ position: [0, 3, 10] }}>
+                <ambientLight />
+                <Compression modelPath={modelPath} onExport={setExportHandler}  />
+                <OrbitControls />
+                <Stats />
+            </Canvas>
+        </>
+    );
 }
